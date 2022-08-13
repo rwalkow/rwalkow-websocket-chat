24b95d3811244b424f638b21f55cd02b1c7090c3
Rafal Walkow <r.walkow@po.edu.pl>
2022-08-14 01:09:17 +0200

    Add server.js

--- client/app.js
+++ client/app.js
@@ -23,15 +23,28 @@ function login(event) {
 function sendMessage(e) {
   e.preventDefault();
 
-  if (!messageContentInput) {
+  if (!messageContentInput.value) {
     alert('Message field can\t be empty!');
   } else {
-    addMessageForm(userName, messageContentInput.value);
+    addMessage(userName, messageContentInput.value);
     messageContentInput.value = '';
   }
 }
 
-function addMessageForm(author, messageContent) {}
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if (author === userName) {
+    message.classList.add('message--self');
+  }
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author}</h3>
+    <div class="message__content">
+      ${content}
+    </div>`;
+  messagesList.appendChild(message);
+}
 
 loginForm.addEventListener('submit', (e) => {
   login(e);
